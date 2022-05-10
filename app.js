d0264c0d0c2ddb8f66d7f562ca45f3adc07837f4
mmwihaki <marymwihaki16@gmail.com>
2022-05-10 13:47:33 -0600

    added login and signup form

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import express from 'express'
 const app = express()
 
 app.set('view engine', 'ejs')
+app.use(express.static('public'))
 
 const colors = ['Red', 'Yellow', 'Blue', 'Green', 'Orange']
 
