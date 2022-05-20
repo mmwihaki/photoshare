f72b45953582209ab900ef9e0b0c24b124b881f1
mmwihaki <marymwihaki16@gmail.com>
2022-05-20 22:34:13 +0300

    added form validation

--- app.js
+++ app.js
@@ -5,20 +5,104 @@ const app = express()
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
-const colors = ['Red', 'Yellow', 'Blue', 'Green', 'Orange']
+//config to access form info
+app.use(express.urlencoded({extended: false}))
+
+const users = [
+    {
+        fullname: 'Mini Me',
+        email: 'kessylyeon@yahoo.com',
+        password: '5yv9vdUseJ@ChU'
+    }
+]
 
 app.get('/', (req, res) => {
-    res.render('index', {title: true, colors: colors})
+    res.render('index', {title: true})
 })
 
+//display login form
 app.get('/login', (req, res) => {
-    res.render('login')
+    let user = {
+        email: '',
+        password: ''
+    }
+    res.render('login', {
+        error: false, user: user})
 })
 
-//create a signup route that sends 'sign up page' as response
+//process login form
+app.post('/login', (req, res) => {
+    let user = users.find(user => user.email === req.body.email)
 
+    if (user) {
+        if (user.password === req.body.password) {
+            console.log('Access granted')
+            res.redirect('/')
+        } else {
+            let user = {
+                email: req.body.email,
+                password: req.body.password
+            }
+            let message = 'Email/Password mismatch'
+            res.render('login', {error: true, message: message, user: user})
+        }
+    } else {
+        let user = {
+            email: req.body.email,
+            password: req.body.password
+        }
+        let message = 'Account does not exist. Please create one.'
+        res.render('login', {error: true, message: message, user: user})
+    }
+}) 
+
+//display signup form
 app.get('/signup', (req, res) => {
-    res.render('signup')
+    let user = {
+        fullname: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    }
+    res.render('signup', {error: false, user: user})
+})
+
+//process sign up form
+app.post('/signup', (req, res) => {
+    if (req.body.password === req.body.confirmPassword) {
+        
+        let user = users.find(user => user.email === req.body.email)
+        if (user) {
+            let user = {
+                fullname: req.body.fullname,
+                email: req.body.email,
+                password: req.body.password,
+                confirmPassword: req.body.confirmPassword
+            }
+            let message = 'Account already exists with the email provided.'
+            res.render('signup', { error: true, message: message, user: user})
+        } else {
+            let user = {
+                fullname: req.body.fullname,
+                email: req.body.email,
+                password: req.body.password
+            }
+            users.push(user)
+            console.log(user)
+            console.log('Account created successfully')
+            res.redirect('/')
+        }
+
+    } else {
+        let user = {
+            fullname: req.body.fullname,
+            email: req.body.email,
+            password: req.body.password,
+            confirmPassword: req.body.confirmPassword
+        }
+        let message = 'Passwords are not matching'
+        res.render('signup', { error: true, message: message, user: user})
+    }
 })
 
 app.listen(3000, () => {
